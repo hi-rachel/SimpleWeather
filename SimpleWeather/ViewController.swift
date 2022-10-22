1ea7903a516ca210042ab57d44bc12f91de6f935
hi-rachel <rachel.uiux@gmail.com>
2022-10-22 17:06:55 +0900

    feat : change city

---
+++ SimpleWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by 문총미 on 2022/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
